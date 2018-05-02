f21a1bd5911cbda3d95a8f8da994d2eb7a31c736
Mauricio Correa <mauricio.correa@macys.com>
2018-05-02 18:41:22 -0300

    1 - Add ngIf attribute to empty state div

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Fancy Burger';
+  public shouldDisplayBlankState;
+
+  constructor() {
+    this.shouldDisplayBlankState = true;
+  }
 }
