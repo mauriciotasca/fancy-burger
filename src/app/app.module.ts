5c4c9b6457bea25cf4e8579ae92a01c7e07b41b4
Mauricio Correa <mauricio.correa@macys.com>
2018-05-08 18:30:13 -0300

    General Improvements on app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,12 @@ import { AppComponent } from './app.component';
 import { MealsComponent } from './meals/meals.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ItemListComponent } from './shared/item-list/item-list.component';
-import { AppRoutingModule } from './/app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
 import { DrinksComponent } from './drinks/drinks.component';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { ItemFilterPipe } from './shared/pipes/item-filter.pipe';
-import {FormsModule} from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
