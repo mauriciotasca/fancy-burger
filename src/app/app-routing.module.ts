ae5724c841122de349c87a3c44222dd82849fcc3
Mauricio Correa <mauricio.correa@macys.com>
2018-05-08 13:35:10 -0300

    Route fix, still waiting on data and drinks route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
-import {DrinksComponent} from './drinks/drinks.component';
+import {mealsRoutes} from './meals/meals.routing';
 import {drinksRoutes} from './drinks/drinks.routing';
 import {homeRoutes} from './home/home.routing';
 import {notFoundRoutes} from './not-found/not-found.routing';
 
 const routes: Routes = [
+  ...mealsRoutes,
   ...drinksRoutes,
   ...homeRoutes,
   ...notFoundRoutes,
---
+++ src/app/meals/meals.routing.ts
@@ -0,0 +1,6 @@
+import {Routes} from '@angular/router';
+import {MealsComponent} from './meals.component';
+
+export const mealsRoutes: Routes = [
+  { path: 'meals', component: MealsComponent }
+];
