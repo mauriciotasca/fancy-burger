20afebbef51478ca17c67fff0adcf9aea808a78e
Mauricio Correa <mauricio.correa@macys.com>
2018-05-14 15:24:02 -0300

    Fix E2E and show brand name

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -7,8 +7,8 @@ describe('fancy-burger App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display brand title', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toEqual('Fancy Burger');
   });
 });
